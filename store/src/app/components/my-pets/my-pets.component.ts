870acb10e44420f563f4acfc71c7d94be583b9e5
Yevhenii Kaliukh <eukalix@gmail.com>
2020-12-29 01:45:05 +0200

    Added styles.

--- store/src/app/components/my-pets/my-pets.component.ts
+++ store/src/app/components/my-pets/my-pets.component.ts
@@ -42,34 +42,4 @@ export class MyPetsComponent implements OnInit {
       console.log(i);
     }
   }
- 
-  /*loadRequests(petId: number): InputRequest[] {
-    console.log('Hello');
-    console.log("Pet ID  "+petId)
-    let requests: InputRequest[];
-    this.requestService.getRequestsByPetId(petId)
-                       .subscribe(pets => requests = pets);
-    console.log('I got' + requests);
-    return requests;
-  }*/
-  /*loadRequests(): void {
-    console.log('im here');
-    for(let i = 0; i < this.pets.length; i++){
-      console.log(this.pets[i].petId);
-    }
-    for(let i = 0; i < this.pets.length; i++){
-      //this.requests[i] = 
-      this.requestService.getRequestsByPetId(this.pets[i].petId)
-                       .subscribe(pets => {this.pets[i].requests = pets;});
-    }
-    console.log('ddd ' + this.pets[0].requests[0])
-    
-    
-    //console.log('Hello');
-    //console.log("Pet ID  "+petId)
-    //let requests: InputRequest[];
-    //this.requestService.getRequestsByPetId(petId)
-    //                   .subscribe(pets => requests = pets);
-    //return requests;
-  }*/
 }
